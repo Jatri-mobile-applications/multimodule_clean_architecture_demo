bb4821c40e0c0db93c2ae8228a2e1a75d4a78a63
kamrulhasan <kamrulhasan3288@gmail.com>
2022-01-17 14:03:00 +0600

    create navigation graph

--- buildSrc/src/main/java/ModulesDep.kt
+++ buildSrc/src/main/java/ModulesDep.kt
@@ -1,34 +1,4 @@
 object ModulesDep {
-    const val assets = ":assets"
-    const val sharedPref = ":library:sharedpref"
-
-    const val apiResponse = ":model:apiresponse"
-    const val entity = ":model:entity"
-
-    const val di = ":core:di"
-    const val api = ":core:api"
-    const val data = ":data"
-    const val domain = ":domain"
-    const val common = ":common"
+    const val postList = ":feature:list"
     const val navigation = ":navigation"
-
-    const val splash = ":feat-credential:splash"
-    const val login = ":feat-credential:login"
-    const val verifyOtp = ":feat-credential:verifyotp"
-    const val registration = ":feat-credential:registration"
-
-    const val digitalBusTicketHome = ":feat-digital-bus-ticket:home"
-    const val digitalBusTicketServices = ":feat-digital-bus-ticket:ticketservices"
-    const val busTicketGuaranteedSeat = ":feat-digital-bus-ticket:guaranteed-seat"
-    const val busTicketAgentBooking = ":feat-digital-bus-ticket:agent-booking"
-    const val digitalBusTicketSlider = ":feat-digital-bus-ticket:bus-slider"
-
-    const val digitalBusTicketStatus = ":feat-digital-bus-ticket:ticketstatus"
-    const val digitalBusTicketConfirmed = ":feat-digital-bus-ticket:ticketconfirmed"
-    const val digitalBusTicketPayment= ":feat-digital-bus-ticket:payment"
-    const val digitalBusTicketPaymentGateway= ":feat-digital-bus-ticket:paymentgateway"
-    const val digitalBusTicketHistoryHome = ":feat-digital-bus-ticket:tickethistoryhome"
-    const val digitalBusTicketHistory = ":feat-digital-bus-ticket:tickethistory"
-
-    const val webView = ":feat-common:webview"
 }
