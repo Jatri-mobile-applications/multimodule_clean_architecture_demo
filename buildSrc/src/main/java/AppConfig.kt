2331d0c6727daceafdf6ad3617da14adb6c7647a
kamrulhasan <kamrulhasan3288@gmail.com>
2022-01-17 13:30:53 +0600

    add dependency

---
+++ buildSrc/src/main/java/AppConfig.kt
@@ -0,0 +1,9 @@
+object AppConfig {
+    const val applicationId = "com.jatri.jatriuser"
+    const val testRunner = "androidx.test.runner.AndroidJUnitRunner"
+    const val minSdkVersion = 21
+    const val compileSdkVersion = 31
+    const val targetSdkVersion = 31
+    const val versionCode  = 78
+    const val versionName  = "2.14.3"
+}
